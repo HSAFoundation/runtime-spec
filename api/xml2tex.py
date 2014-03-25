fe3bdaedca71b594a64a01f1e4fbe683b7b53148
Mario Mendez-Lojo <Mario.MendezLojo@amd.com>
2014-03-25 13:48:47 -0600

    v0.178 Fixed @details bug
    All the text beyond the first paragraphs in @details was being lost.
    Fixed several other problems in the Latex generation, mainly by introducing breakable boxes/tables (otherwise the formatting is awkward).
    removed API,STR,ENU convention.
    
    From now on, the document name contains the version name.
    
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/runtimenew/doc/spec/": change = 1020301]

--- api/xml2tex.py
+++ api/xml2tex.py
@@ -6,7 +6,8 @@ import os
 
 # escape characters to make them Latex-friendly
 def text2tex(text):
-  return text.replace("_", "\\_")
+  text = text.replace("_", "\\_")
+  return text
 
 # custom text (Latex) iterator that consumes XML
 def myitertext(self):
@@ -96,16 +97,16 @@ def process_file(file):
       continue
     tex.write("\n\n")
     # begin box
-    tex.write('\\noindent\\begin{tcolorbox}[nobeforeafter,arc=0mm,colframe=white,colback=lightgray,left=0mm]\n')
+    tex.write('\\noindent\\begin{tcolorbox}[breakable,nobeforeafter,arc=0mm,colframe=white,colback=lightgray,left=0mm]\n')
     # typedef name
     tex.write(typ + " \\hsadef{" + typedef.get('id') + "}")
-    tex.write("{" + node2tex(typedef.find('name')) + "} \\\\" + "\n")
+    tex.write("{" + node2tex(typedef.find('name')) + "}" + "\n")
     # members. Doxygen stores their info in a separate file.
     membersfile = typedef.find('type/ref').get('refid') + ".xml"
     memberstree = ET.parse(os.path.join('xml',membersfile))
     membersroot = memberstree.getroot()
     members = membersroot.findall(".//memberdef[@kind='variable']")
-    tex.write("\\begin{tabular}{@{}l}" + "\n")
+    tex.write("\\vspace{-3.5mm}\\begin{longtable}{@{}p{\\textwidth}}" + "\n")
     vals = []
     fields = []
     for member in members:
@@ -123,7 +124,7 @@ def process_file(file):
       txt += node2tex(member.find('detaileddescription/para'))
       fields.append(txt)
     tex.write("\\\\\n".join(vals))
-    tex.write("\n\\end{tabular}" + "\n\n")
+    tex.write("\n\\end{longtable}" + "\n\n")
     # end box
     tex.write('\\end{tcolorbox}\n')
     # brief
@@ -139,7 +140,7 @@ def process_file(file):
   for func in funcs:
     tex.write("\n\n")
     # begin box
-    tex.write('\\noindent\\begin{tcolorbox}[nobeforeafter,colframe=white,colback=lightgray,left=0mm]\n')
+    tex.write('\\noindent\\begin{tcolorbox}[breakable,nobeforeafter,colframe=white,colback=lightgray,left=0mm]\n')
     # signature - return value
     tex.write(node2tex(func.find('type')) + " ")
     # signature - func name
@@ -148,15 +149,15 @@ def process_file(file):
     # signature - parameters
     sigargs = func.findall("param")
     if sigargs:
-      tex.write("\\\\\n")
-      tex.write("\\begin{tabular}{@{}l}" + "\n")
+      tex.write("\n")
+      tex.write("\\vspace{-3.5mm}\\begin{longtable}{@{}p{\\textwidth}}" + "\n")
       arglst = []
       for arg in sigargs:
         argtxt = "\\hspace{1.7em}" + node2tex(arg.find('type'))
         argtxt += " \\hsaarg{" + node2tex(arg.find('declname')) + "}"
         arglst.append(argtxt)
       tex.write(",\\\\\n".join(arglst))
-      tex.write(")\\end{tabular}")
+      tex.write(")\\end{longtable}")
     else:
       tex.write(")")
     tex.write("\n\n")
@@ -167,7 +168,7 @@ def process_file(file):
     # parameters
     args = func.findall(".//parameterlist[@kind='param']/parameteritem")
     if args:
-      tex.write("\\noindent\\textbf{Parameters}\\\\[-5mm]" + "\n")
+      tex.write("\\noindent\\textbf{Parameters}\\\\[-6mm]" + "\n")
       tex.write("\\noindent\\begin{longtable}{@{}>{\\hangindent=2em}p{\\textwidth}}" + "\n")
       arglst = []
       for arg in args:
@@ -181,7 +182,7 @@ def process_file(file):
       tex.write("\\\\[2mm]\n".join(arglst))
       tex.write("\n\\end{longtable}" + "\n")
     else:
-      # add the tabular anyway so margin are kept constant
+      # add the longtable anyway so margin are kept constant
       tex.write("\\noindent\\begin{longtable}{@{}>{\\hangindent=2em}p{\\textwidth}}" + "\n")
       tex.write("\n\\end{longtable}" + "\n")
     # return values
@@ -189,7 +190,7 @@ def process_file(file):
     if rets:
       if args:
         tex.write('\\vspace{-5mm}')
-      tex.write("\\noindent\\textbf{Return Values}\\\\[-5mm]" + "\n")
+      tex.write("\\noindent\\textbf{Return Values}\\\\[-6mm]" + "\n")
       tex.write('\\noindent\\begin{longtable}{@{}>{\\hangindent=2em}p{\\linewidth}}' + "\n")
       arglst = []
       for ret in rets:
@@ -204,15 +205,21 @@ def process_file(file):
       tex.write('\\vspace{-5mm}')
       tex.write("\\noindent\\textbf{Returns}\\\\[1mm]"+ "\n")
       tex.write(node2tex(ret) + "\n\n")
-      # add the tabular anyway so margin are kept constant
+      # add the longtable anyway so margin are kept constant
       tex.write('\\noindent\\begin{longtable}{@{}>{\\hangindent=2em}p{\\linewidth}}' + "\n")
       tex.write("\n\\end{longtable}" + "\n")
+
     # description
-    para = func.find('detaileddescription/para')
-    if para is not None and para.find('parameterlist') is None:
-      tex.write('\\vspace{-5mm}')
-      tex.write("\\noindent\\textbf{Description}\\\\"+ "\n")
-      tex.write(node2tex(para))
+    paras = func.findall('detaileddescription/para')
+    paraslst = []
+    for para in paras:
+      # information about parameters is also in the detaileddescription, so skip that
+      if para.find('parameterlist') is None:
+        paraslst.append(node2tex(para))
+    if paraslst:
+      tex.write('\\vspace{-4mm}')
+      tex.write("\\noindent\\textbf{Description}\\\\[1mm]"+ "\n")
+      tex.write("\\\\[2mm]\n".join(paraslst))
     tex.write(" \n")
   tex.close()
 
@@ -221,10 +228,12 @@ def main():
   if not os.path.exists(outdir):
     os.makedirs(outdir)
   for file in os.listdir('xml'):
-    if file.find("group__API__") == 0 or file.find("group__ENU__") == 0 or file.find("group__STR__") == 0 or file.find("group__TDF__") == 0:
-      sys.stdout.write('Processing ' + file + "...")
-      process_file(file)
-      print('OK')
+    if file.find("group__") != 0:
+      # other files(ex: structs) are recursively processed through their group
+      continue
+    sys.stdout.write('Processing ' + file + "...")
+    process_file(file)
+    print('OK')
   return
 
 if __name__ == "__main__":
